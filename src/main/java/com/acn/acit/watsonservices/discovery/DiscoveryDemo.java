02c65535efb264b0c7bafc86aa922ae8bcc638fd
Hizon <fatima.h.c.hizon@accenture.com>
2018-01-15 16:20:20 +0800

    Update service credentials

--- src/main/java/com/acn/acit/watsonservices/discovery/DiscoveryDemo.java
+++ src/main/java/com/acn/acit/watsonservices/discovery/DiscoveryDemo.java
@@ -9,9 +9,9 @@ public class DiscoveryDemo {
 	public static void main(String[] args) {
 		Discovery discovery = new Discovery("2017-11-07");
 		discovery.setEndPoint("https://gateway.watsonplatform.net/discovery/api/");
-		discovery.setUsernameAndPassword("6780436a-0ed6-4b16-80e7-7e6ac8bc2942", "azLpDfMj07MM");
-		String environmentId = "18564e33-0ab0-46ed-860f-7b2e9d93cc14";
-		String collectionId = "0026cbdc-9e9a-4d4e-8f1f-ef1f39f5fd53";
+		discovery.setUsernameAndPassword("b27ca3a2-ccee-43c0-ab97-90b758a480d3", "UTfvlaUg1DlZ");
+		String environmentId = "5f4d2912-39c4-4f8d-b38a-ee5c228457f8";
+		String collectionId = "16de0c9d-d456-457b-92d9-4075cae65f2c";
 
 		QueryRequest.Builder queryBuilder = new QueryRequest.Builder(environmentId, collectionId);
 		queryBuilder.query("enriched_text.entities.text:IBM");
